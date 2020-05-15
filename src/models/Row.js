c32f74c33dc72fdd4379a06eaebd789645256b7d
Matt Richmond <matthew.richmond@wowza.com>
2020-05-14 20:11:06 -0600

    model updates

--- src/models/Row.js
+++ src/models/Row.js
@@ -1,6 +1,7 @@
 import { RowResults } from "./RowResults";
 import { Code } from "./Code";
 import { Guess } from "./Guess";
+import { Yellow } from "./pieces";
 
 export class Row {
   constructor(code) {
@@ -36,10 +37,65 @@ export class Row {
    * Compares this Row's Code against the answerRow's Code.
    *
    * @param {Row} answerRow
+   *
+   * @returns bool Whether or not the rows match entirely
    */
   compare(answerRow) {
-    console.debug(answerRow.getCode(), this.getCode());
-    return this.getCode().compare(answerRow.getCode());
+    this.compareForExactMatches(answerRow);
+    this.compareForColorMatches(answerRow);
+    return this.results.isEntireMatch();
+  }
+
+  compareForExactMatches(answerRow) {
+    let code = this.getCode(),
+        exactMatches = [];
+    _.each(code.retrieve(), (placement) => {
+      let answerPlacement = answerRow.getCode().retrieve()[placement.index - 1];
+      if (placement.piece.color === answerPlacement.piece.color) {
+        exactMatches.push(placement);
+      }
+    });
+    this.results.storeCorrectPlacements(exactMatches);
+  }
+
+  compareForColorMatches(answerRow) {
+    let guessCountByColor = null,
+        guessUniqueColors = null,
+        answerCountByColor = null,
+        answerUniqueColors = null,
+        matchingColors = null,
+        colorMatches = {};
+
+    answerCountByColor = _.countBy(answerRow.getCode().retrieve(), (placement) => {
+      return placement.piece.color;
+    });
+    answerUniqueColors = _.uniq(_.map(answerRow.getCode().retrieve(), (placement) => {
+      return placement.piece.color;
+    }));
+    guessCountByColor = _.countBy(this.getCode().retrieve(), (placement) => {
+      return placement.piece.color;
+    });
+    guessUniqueColors = _.uniq(_.map(this.getCode().retrieve(), (placement) => {
+      return placement.piece.color;
+    }));
+
+
+    matchingColors = _.intersection(guessUniqueColors, answerUniqueColors);
+
+    /**
+     * General syntax of object:
+     * colorMatches = {
+     *   yellow: 5,
+     *   red: 1,
+     * }
+     */
+    _.each(matchingColors, (color) => {
+      colorMatches[color] = (guessCountByColor[color] >= answerCountByColor[color])
+        ? answerCountByColor[color]
+        : guessCountByColor[color];
+    });
+
+    this.results.storeCorrectColorCount(_.sum(_.values(colorMatches)));
   }
 
   /**
--- src/models/RowResults.js
+++ src/models/RowResults.js
@@ -1,7 +1,20 @@
+import * as Config from './../config';
+
 export class RowResults {
   constructor() {
     this.correctColors = 0;
     this.correctPlacements = 0;
   }
 
+  storeCorrectPlacements(placements) {
+    this.correctPlacements = placements.length;
+  }
+
+  storeCorrectColorCount(totalCount) {
+    this.correctColors = (totalCount - this.correctPlacements);
+  }
+
+  isEntireMatch() {
+    return this.correctPlacements === Config.CODE_LENGTH;
+  }
 }
